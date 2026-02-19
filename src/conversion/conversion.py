f0f1dbf5a3f8a42064eaa9547713fdfbcac74c7f
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-02-19 15:43:05 -0500

    test de binario_a_decimal funciona

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -100,7 +100,7 @@ class Conversion:
             binario_a_decimal("1010") -> 10
             binario_a_decimal("11111111") -> 255
         """
-        pass
+        return int(binario, 2) # Convertir de binario a decimal usando base 2 (+w+)
     
     def decimal_a_romano(self, numero):
         """
