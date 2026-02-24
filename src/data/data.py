f993659bc4e91609ec1c1bf343756f058b58644b
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-02-23 23:12:44 -0500

    .

--- src/data/data.py
+++ src/data/data.py
@@ -141,7 +141,7 @@ cunado funciono https://youtu.be/K-gbAi5UIg0?si=60H9YhF8_Dz3zESB
             lista (list): Lista de enteros del 1 al n con un número faltante
             
         Returns:
-            int: El número que falta en la secuencia
+            int: El número que falta en la secuencia (°w°)
         """
         n = len(lista) + 1  # El número total de elementos debería ser n
         suma_esperada = n * (n + 1) // 2  # Suma de los primeros n números
