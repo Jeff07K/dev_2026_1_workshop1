7658e4c33c1ae4e70c98c7a3dc58c81ff7775616
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-03-10 11:47:49 -0500

    comenzamos con magic.py y el test fibonaci funciono : https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwyzU0ZzEINqjOMJ5hZpHo-3pdnjiX70ohuA&s

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -14,7 +14,17 @@ class Magic:
         Returns:
             int: El n-ésimo número de Fibonacci
         """
-        pass
+        if n < 0:
+            return None
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
     
     def secuencia_fibonacci(self, n):
         """
