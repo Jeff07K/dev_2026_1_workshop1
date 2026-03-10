5533d8ff6503e7574ab6bd62acee8e98851fb65b
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-03-10 11:55:47 -0500

    el truma de calculo funciona (factorizar)

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -130,7 +130,15 @@ class Magic:
         Returns:
             int: El factorial de n
         """
-        pass
+        if n < 0:
+            return None
+        elif n == 0 or n == 1:
+            return 1
+        else:
+            resultado = 1
+            for i in range(2, n + 1):
+                resultado *= i
+            return resultado
     
     def mcd(self, a, b):
         """
