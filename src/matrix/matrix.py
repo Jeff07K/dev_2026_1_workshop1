378831c13cf5782d62b56de48bb873d0fa4301f9
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-03-12 11:19:21 -0500

    determinante_3x3 tests

--- src/matrix/matrix.py
+++ src/matrix/matrix.py
@@ -262,7 +262,14 @@ class Matrix:
             determinante_3x3([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) -> 0
             determinante_3x3([[1, 0, 0], [0, 2, 0], [0, 0, 3]]) -> 6
         """
-        pass
+        if len(matriz) != 3 or len(matriz[0]) != 3:
+         raise ValueError("La matriz debe ser 3x3")
+    
+        a = matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[1][2] * matriz[2][1])
+        b = matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[1][2] * matriz[2][0])
+        c = matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[1][1] * matriz[2][0])
+    
+        return a - b + c
 
     def identidad(self, n):
         """
