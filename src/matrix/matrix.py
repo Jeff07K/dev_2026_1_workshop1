b1885882d9b336abb45fd431b6629ffae9691874
Jeff07K <jabejarano09@ucatolica.edu.co>
2026-03-12 10:48:22 -0500

    suma_matrices test

--- src/matrix/matrix.py
+++ src/matrix/matrix.py
@@ -21,7 +21,18 @@ class Matrix:
         Ejemplo:
             suma_matrices([[1, 2], [3, 4]], [[5, 6], [7, 8]]) -> [[6, 8], [10, 12]]
         """
-        pass
+        if len(A) != len(B) or len(A[0]) != len(B[0]):
+            raise ValueError("Dimensiones incompatibles")
+
+        resultado = []
+
+        for i in range(len(A)):
+            fila = []
+            for j in range(len(A[0])):
+                fila.append(A[i][j] + B[i][j])
+            resultado.append(fila)
+
+        return resultado
 
     def resta_matrices(self, A, B):
         """
